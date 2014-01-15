656b7e2a6fb3fab243a45bbd1d6008113b98e55c
Andrew Bell <andrew@recursivechaos.com>
2014-01-15 16:04:34 -0600

    Created the mainBotConsole, the UrlAnnounce plugin and the social plugin, both are buggy, at best.

---
+++ src/main/java/com/recursivechaos/lcbot/bot/MainBotConsole.java
@@ -0,0 +1,63 @@
+package main.java.com.recursivechaos.lcbot.bot;
+
+/**
+ * MainBotConsole is responsible for initializing the bot object. Each 
+ * bot module needs to have their listener attached.
+ * 
+ * @author Andrew Bell
+ *
+ */
+import java.io.IOException;
+
+import main.java.com.recursivechaos.lcbot.plugins.social.SocialPluginListener;
+import main.java.com.recursivechaos.lcbot.plugins.urlAnnounce.UrlAnnouncePluginListener;
+
+import org.pircbotx.Configuration;
+import org.pircbotx.PircBotX;
+import org.pircbotx.UtilSSLSocketFactory;
+import org.pircbotx.cap.TLSCapHandler;
+import org.pircbotx.exception.IrcException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainBotConsole {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args) {
+		// Start logger
+		Logger logger = LoggerFactory.getLogger(MainBotConsole.class);
+		logger.info("Loading bot configuration.");
+		// Load configuration
+		// TODO: Register with nickserv
+		Configuration configuration = new Configuration.Builder()
+				.setName("LCbot")
+				// Set the nick of the bot. CHANGE IN YOUR CODE
+				.setLogin("LCbot")
+				// login part of hostmask, eg name:login@host
+				.setAutoNickChange(true)
+				// Automatically change nick
+				.setCapEnabled(true)
+				// Enable CAP features
+				.addCapHandler(
+						new TLSCapHandler(new UtilSSLSocketFactory()
+								.trustAllCertificates(), true))
+				.addListener(new SocialPluginListener())
+				.addListener(new UrlAnnouncePluginListener())
+				.setServerHostname("chat.freenode.org")
+				.addAutoJoinChannel("#LaunchCode").buildConfiguration();
+		logger.info("Configuration loaded.");
+		// Start bot
+		try {
+			PircBotX bot = new PircBotX(configuration);
+			bot.startBot();
+			logger.info("Bot Started.");
+		} catch (IOException e) {
+			e.printStackTrace();
+			logger.error("IOException: " + e.getMessage());
+		} catch (IrcException e) {
+			e.printStackTrace();
+			logger.error("IrcException: " + e.getMessage());
+		} finally {
+			logger.info("Bot stopped.");
+		}
+	}
+}
---
+++ src/main/java/com/recursivechaos/lcbot/plugins/urlAnnounce/UrlBO.java
@@ -0,0 +1,121 @@
+package main.java.com.recursivechaos.lcbot.plugins.urlAnnounce;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.pircbotx.hooks.events.MessageEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("rawtypes")
+public class UrlBO {
+	static final String[] validUrls = { "http://", "www.", "reddit.com/",
+			"redd.it/" };
+	Logger logger = LoggerFactory.getLogger(UrlBO.class);
+
+	/**
+	 * Gets a string describing the URL given
+	 * 
+	 * @param event
+	 *            event containing a URL
+	 * @return string description of URL
+	 */
+	public String getAnnouncement(MessageEvent event) {
+		String url = getUrlFromEvent(event);
+		String title = getPreviewFromUrl(url);
+		return title;
+	}
+
+	/**
+	 * Returns preview text for a url, depending on the source
+	 * 
+	 * @param url
+	 *            string of the url
+	 * @return preview text of the url
+	 */
+	private String getPreviewFromUrl(String url) {
+		String preview = null;
+		if (url.contains("reddit.com")) {
+			RedditUrlBO reddit = new RedditUrlBO();
+			try {
+				preview = reddit.getPreview(url);
+			} catch (Exception e) {
+				logger.info("Was not able to parse URL: " + e.getMessage());
+			}
+		}
+		return preview;
+	}
+
+	/**
+	 * Returns URL after checking to see if it redirects
+	 * 
+	 * @param url
+	 *            string value of a url
+	 * @return string value of the redirected URL
+	 */
+	private String getRedirectedURL(String url) {
+		logger.info("Predirected url: " + url);
+		// If redirect, finds end url
+		HttpURLConnection connection;
+		try {
+			connection = (HttpURLConnection) new URL(url).openConnection();
+			connection.setInstanceFollowRedirects(false);
+			while (connection.getResponseCode() / 100 == 3) {
+				url = connection.getHeaderField("location");
+				logger.info("Long URL: " + url);
+				connection = (HttpURLConnection) new URL(url).openConnection();
+				connection.setInstanceFollowRedirects(false);
+			}
+		} catch (MalformedURLException e) {
+			logger.info("Malformed URL: " + e.getMessage());
+			url = null;
+		} catch (IOException e) {
+			logger.info("IOException: " + e.getMessage());
+			url = null;
+		}
+		logger.info("Redirected URL: " + url);
+		return url;
+	}
+
+	/**
+	 * Fetches the URL string from a message event
+	 * 
+	 * @param event
+	 *            from pircbotx
+	 * @return String of the url in the event
+	 */
+	private String getUrlFromEvent(MessageEvent event) {
+		// stripped URL
+		String url = "";
+		// get message text
+		String message = event.getMessage().toLowerCase();
+		// start location of URL
+		int start = -1;
+		// search message for URL
+		for (String u : validUrls) {
+			if (message.contains(u)) {
+				// If earlier, or not found
+				if ((start > message.indexOf(u)) || (start == -1)) {
+					start = message.indexOf(u);
+					logger.debug("Index found at " + start);
+				}
+			}
+		}
+		// If a URL is found, trim url
+		if (start != -1) {
+			String ltrim = message.substring(start);
+			int end = ltrim.indexOf(" ");
+			if (end != -1) {
+				url = ltrim.substring(0, end);
+			} else {
+				url = ltrim;
+			}
+
+		}
+		url = getRedirectedURL(url);
+		return url;
+	}
+
+}
